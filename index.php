0baa2fe93714b1fc87e330b75b146f2644baa1c0
bogerhing <bogerhinglevd162@gmail.com>
2026-01-30 10:18:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <!-- <meta charset="UTF-8">
-    <title>Redirecting...</title>
-
-    <!-- Instant Redirect -->
-    <meta http-equiv="refresh" content="0; url=https://hammerhead-app-8o8fk.ondigitalocean.app/"> -->
+  <!-- <script>
+    window.location.replace("https://ioerr-app-guwjw.ondigitalocean.app");
+</script> -->
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -881,6 +879,7 @@ function toggleMenu(){
 
 </body>
 </html>
+
 
 
 
